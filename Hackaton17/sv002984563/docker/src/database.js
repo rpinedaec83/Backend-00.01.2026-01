5fd91f1be04c3fa2534dce634761642213e95a5d
Keterlyn <keterlyn.moreno@gmail.com>
2026-05-30 15:03:50 -0500

    Hackaton17

--- Hackaton17/sv002984563/docker/src/database.js
+++ Hackaton17/sv002984563/docker/src/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-    process.env.DB_SCHEMA || 'postgres',
-    process.env.DB_USER || 'postgres',
-    process.env.DB_PASSWORD || 'Z@rverus11',
+    process.env.DB_SCHEMA, 
+    process.env.DB_USER,  
+    process.env.DB_PASSWORD
     {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || 5432,
+        host: process.env.DB_HOST, 
+        port: process.env.DB_PORT,
         dialect: 'postgres'
     }
 );
